1c0aa038fdfee820d126c6d61c053f478224dc82
rcampux <r.camposdesign@gmail.com>
2025-03-28 19:41:21 -0300

    If else e comando Break loop

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
+console.log(numeroSecreto);
 let chute //espaço para armazenar o valor do chute na linha 8
+let tentativas = 1;
 
 /* Enquanto chute for não igual (!=), ao número secreto */
 while (chute != numeroSecreto ) {
-    chute = prompt ('Escolha um número entre 1 e 10');
     /* Se o chute for igual au número secreto */
+    chute = prompt ('Escolha um número entre 1 e 10');
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+        break
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        /* tentativas = tentativas +1; */
+        tentativas++;
     }
+}
+if (tentativas > 1) {
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
 }
 
